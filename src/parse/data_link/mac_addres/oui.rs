47048a55fadcab4f399df8323daf05a437ecde1a
akmot9 <avicocyprien@yahoo.com>
2025-02-11 15:06:06 +0100

    licence

--- src/parse/data_link/mac_addres/oui.rs
+++ src/parse/data_link/mac_addres/oui.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 Cyprien Avico avicocyprien@yahoo.com
+//
+// Licensed under the MIT License <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed except according to those terms.
+
+
 //! The `Oui` module provides a representation of Organizationally Unique Identifiers (OUI).
 //! An OUI is the first 3 bytes of a MAC address, used to identify the manufacturer of a device.
 //!
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,3 +1,8 @@
+// Copyright (c) 2024 Cyprien Avico avicocyprien@yahoo.com
+//
+// Licensed under the MIT License <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed except according to those terms.
+
 pub mod application;
 pub mod data_link;
 // You can determine either a full raw packet that will return a PacketParsed struct composed of data link network transportand application layers.
