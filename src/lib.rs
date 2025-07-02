c4f3e36d91491faa92083b45d68554df0bb1bf96
akmot9 <avicocyprien@yahoo.com>
2025-07-02 15:39:54 +0200

    im so fast

--- src/lib.rs
+++ src/lib.rs
@@ -114,5 +114,10 @@ pub use parse::data_link::DataLink;
 /// Exports MAC address parsing functionality.
 pub use parse::data_link::mac_addres::MacAddress;
 
+
+pub use parse::internet::Internet;
+pub use parse::transport::Transport;
+pub use parse::application::Application;
+
 /// Exports data link layer parsing functionality.
 pub use parse::PacketFlow;
