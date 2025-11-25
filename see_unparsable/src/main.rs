90d483279b40b562d63a45707909353783556a9b
akmot9 <avicocyprien@yahoo.com>
2025-11-25 09:54:06 +0100

    see unparsable

---
+++ see_unparsable/src/main.rs
@@ -0,0 +1,86 @@
+use packet_parser::parse::PacketFlow;
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{self, NetworkInterface};
+use std::convert::TryFrom;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PacketCaptureError {
+    #[error("Interface {0} not found")]
+    InterfaceNotFound(String),
+
+    #[error("Failed to create channel: {0}")]
+    ChannelCreationError(String),
+
+    #[error("Failed to receive packet: {0}")]
+    PacketReceiveError(String),
+
+    #[error("Failed to parse packet: {0}")]
+    PacketParseError(String),
+}
+
+fn find_interface(interface_name: &str) -> Result<NetworkInterface, PacketCaptureError> {
+    let interfaces = datalink::interfaces();
+    interfaces
+        .into_iter()
+        .find(|iface| iface.name == interface_name)
+        .ok_or_else(|| PacketCaptureError::InterfaceNotFound(interface_name.to_string()))
+}
+
+fn create_channel(
+    interface: &NetworkInterface,
+) -> Result<
+    (
+        Box<dyn datalink::DataLinkSender>,
+        Box<dyn datalink::DataLinkReceiver>,
+    ),
+    PacketCaptureError,
+> {
+    match datalink::channel(interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => Ok((tx, rx)),
+        Ok(_) => Err(PacketCaptureError::ChannelCreationError(
+            "Unhandled channel type".to_string(),
+        )),
+        Err(e) => {
+            eprintln!(
+                "use sudo setcap cap_net_raw,cap_net_admin=eip target/debug/integration_test"
+            );
+            Err(PacketCaptureError::ChannelCreationError(e.to_string()))
+        }
+    }
+}
+
+fn main() -> Result<(), PacketCaptureError> {
+    // Sélectionner l'interface réseau
+    let interface_name = "veth0"; // Exemple d'interface : wlp6s0, wlp0s20f3, enxfeaa81e86d1e, veth0
+
+    let interface = find_interface(interface_name)?;
+
+    // Créer un canal pour recevoir les paquets
+    let (_tx, mut rx) = create_channel(&interface)?;
+
+    loop {
+        // Recevoir un paquet
+        let packet = match rx.next() {
+            Ok(packet) => packet,
+            Err(e) => {
+                eprintln!(
+                    "Failed to receive packet: {}",
+                    PacketCaptureError::PacketReceiveError(e.to_string())
+                );
+                continue;
+            }
+        };
+
+        // Tenter de parser le paquet
+        if let Err(e) = PacketFlow::try_from(packet) {
+            let err = PacketCaptureError::PacketParseError(e.to_string());
+
+            eprintln!("--------------");
+            eprintln!("Error parsing packet: {:?}", err);
+            eprintln!("Offending packet (hex): {:02X?}", packet);
+        }
+
+        // Si le parsing est OK, on ne log rien
+    }
+}
